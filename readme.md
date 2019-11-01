3f4cbd266686a7c8ac36e1b75934a0eafe8bff73
Lars <larsmackenzie@gmail.com>
2019-11-01 10:06:20 -0400

    project setup

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Master Course Tracker
+
+Some Google Scripts to help automate information sharing across Google Sheets!
---
+++ scripts.js
@@ -0,0 +1,38 @@
+var updated = [];
+var sheetsList = [];
+
+function getLatestTerms() {
+  for (var j = 1; j < sheetsList.length; j++){
+    var sheet = SpreadsheetApp.getActiveSheet().getRange(sheetsList[j] + '!A2:D150').getValues();
+  for(var i = 0; i < sheet.length; i++){
+    if (sheet[i][3] === true){
+      updated.push({term: sheet[i][2], index: i, course: sheet[i][0], year: sheetsList[j]});
+      }
+    
+    }
+  }
+  getAll();
+  }
+
+function getAll(){
+  var sheet = SpreadsheetApp.getActiveSheet().getRange('Course_List!A2:A142').getValues();
+  for (var i = 0; i < updated.length; i++){
+    for (var j = 0; j < sheet.length; j++){
+      if (updated[i].course === sheet[j][0]){
+        SpreadsheetApp.getActiveSheet().getRange('Course_List!C' + j+2).setValue(updated[i].term + ' ' + updated[i].year);
+        Logger.log('match');
+      }
+    }
+  }
+}
+function getSheets(){
+  var sheets = SpreadsheetApp.getActiveSpreadsheet().getSheets();
+  for (var i = 0; i < sheets.length; i++){
+    sheetsList.push(sheets[i].getName());
+  }
+  getLatestTerms();
+}
+
+function onEdit(){
+  getSheets();
+}
